8d690c3866715d0f3d9217bc2fd41fe27b391627
PulkitBajpai <pulkit.dcd@gmail.com>
2017-05-18 10:34:31 +0530

    changes for cloudtag

--- wordfreq.py
+++ wordfreq.py
@@ -2,24 +2,30 @@ __author__ = 'pulkit-bajpai'
 
 from cleanwords import cleanswords
 
-with open("file2.txt") as f:
-     wordstring = f.read()
+def general_function():
 
-word_list = cleanswords(wordstring)
+    with open("file2.txt") as f:
+         wordstring = f.read()
 
-word_freq = []
-for w in word_list:
-    word_freq.append(word_list.count(w))
+    word_list = cleanswords(wordstring)
 
+    word_freq = []
+    for w in word_list:
+        word_freq.append(word_list.count(w))
+
+    print word_list
+
+
+    with open("file2.txt") as f:
+         noise_tring = f.read()
+
+    noise_words_list = noise_tring.split()
+    final_result = []
+    for word in word_list:
+        if not word in noise_words_list:
+            final_result.append(word)
+    return final_result,zip(word_list, word_freq)
+
+final_result,word_list= general_function()
 print word_list
-print(zip(word_list, word_freq))
-
-with open("file2.txt") as f:
-     noise_tring = f.read()
-
-noise_words_list = noise_tring.split()
-final_result = []
-for word in word_list:
-    if not word in noise_words_list:
-        final_result.append(word)
 print final_result
